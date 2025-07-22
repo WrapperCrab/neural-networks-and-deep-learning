ea2aeaf9d62b999e9b415811fcc77f7b90298f73
Cross, Winter Isaac <wi928rsnowflake@gmail.com>
2025-07-22 16:58:07 -0400

    Implemented matrix-based alg

--- src/network.py
+++ src/network.py
@@ -16,6 +16,7 @@ import time
 
 # Third-party libraries
 import numpy as np
+from PIL import Image
 
 
 class Network(object):
@@ -62,7 +63,7 @@ class Network(object):
                 training_data[k:k+mini_batch_size]
                 for k in range(0, n, mini_batch_size)]
             for mini_batch in mini_batches:
-                self.update_mini_batch(mini_batch, eta)
+                self.update_mini_batch_matrix(mini_batch, eta)
             time2 = time.time()
             if test_data:
                 print("Epoch {0}: {1} / {2}, took {3:.2f} seconds".format(
@@ -86,6 +87,53 @@ class Network(object):
         self.biases = [b-(eta/len(mini_batch))*nb
                        for b, nb in zip(self.biases, nabla_b)]
 
+    def update_mini_batch_matrix(self, mini_batch, eta): # After trying and failing, I found some working code online
+        m = len(mini_batch)
+
+        # transform to (input x batch_size) matrix
+        X = np.asarray([_x.ravel() for _x, _y in mini_batch]).transpose() # Very efficient way of combining input into one matrix ig
+        # transform to (output x batch_size) matrix
+        Y = np.asarray([_y.ravel() for _x, _y in mini_batch]).transpose()
+
+        nabla_b, nabla_w = self.backprop_matrix(X, Y)
+        self.weights = [w - (eta / m) * nw for w, nw in zip(self.weights, nabla_w)]
+        self.biases = [b - (eta / m) * nb for b, nb in zip(self.biases, nabla_b)]
+
+    def backprop_matrix(self, X, Y):
+        nabla_b = [0 for i in self.biases] # Add the actual matrices later
+        nabla_w = [0 for i in self.weights]
+
+        # feedforward
+        activation = X
+        activations = [X] # list to store all the activations, layer by layer
+        Zs = [] # list to store all the z vectors, layer by layer
+        for b, w in zip(self.biases, self.weights):
+            Z = np.dot(w, activation) + np.kron(b, np.ones([1, Y.shape[1]])) # I had found a different, 3 line way to make the B matrix
+            Zs.append(Z)
+            activation = sigmoid(Z)
+            activations.append(activation)
+
+        # backward pass
+        delta = self.cost_derivative(activations[-1], Y) * sigmoid_prime(Zs[-1])
+        nabla_b[-1] = np.sum(delta, axis=1).reshape([len(delta), 1]) # reshape to (n x 1) matrix # This makes sense when you remember we find sum over deltas to get new b values
+        nabla_w[-1] = np.dot(delta, activations[-2].transpose()) # This is  the part I couldn't figure out
+
+        for l in range(2, self.num_layers):
+            Z = Zs[-l]
+            sp = sigmoid_prime(Z)
+            delta = np.dot(self.weights[-l + 1].transpose(), delta) * sp
+            nabla_b[-l] = np.sum(delta, axis=1).reshape([len(delta), 1]) # reshape to (n x 1) matrix
+            nabla_w[-l] = np.dot(delta, activations[-l - 1].transpose()) # This is  the part I couldn't figure out
+
+        # I'm guessing the nabla_w part is the biggest time saver. 
+        # The dot product has sum baked into it. It has a property that the product of the combined vector matrices
+        # is equal to the sum of the matrices multiplied apart
+        # [d_1 d_2][a_1 a_2]^T = d_1 a_1^T + d_2 a_2^T
+        # Why does this property feel so unfamiliar to me?
+
+        return (nabla_b, nabla_w)
+
+
     def backprop(self, x, y):
         """Return a tuple ``(nabla_b, nabla_w)`` representing the
         gradient for the cost function C_x.  ``nabla_b`` and
@@ -105,6 +153,7 @@ class Network(object):
         # backward pass
         delta = self.cost_derivative(activations[-1], y) * \
             sigmoid_prime(zs[-1])
+
         nabla_b[-1] = delta
         nabla_w[-1] = np.dot(delta, activations[-2].transpose())
         # Note that the variable l in the loop below is used a little
@@ -131,15 +180,31 @@ class Network(object):
         return sum(int(x == y) for (x, y) in test_results)
 
     def cost_derivative(self, output_activations, y):
-        """Return the vector of partial derivatives \partial C_x /
-        \partial a for the output activations."""
+        """Return the vector of partial derivatives \ partial C_x /
+        \ partial a for the output activations."""
         return (output_activations-y)
+        # return -(output_activations-y) # TEST negating cost function
+
+    def evaluate_image(self, path):
+        """Runs the network on the passed image at path after converting it to useable data
+        Prints the resulting activations in the output layer"""
+        image = Image.open(path).convert("L")
+        data = np.array(image)
+        data = (255-data) / 255.0
+
+        data = np.reshape(data, (784,1))
+        a = self.feedforward(data)
+
+        print(a)
+        print(np.argmax(a))
 
 #### Miscellaneous functions
 def sigmoid(z):
     """The sigmoid function."""
     return 1.0/(1.0+np.exp(-z))
+    # return (1.0/np.pi)*(np.atan(z)) + 0.5
 
 def sigmoid_prime(z):
     """Derivative of the sigmoid function."""
     return sigmoid(z)*(1-sigmoid(z))
+    # return (1.0/np.pi)*(1/(1 + np.square(z)))
